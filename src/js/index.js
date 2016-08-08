f13bd923b358900b7dd138149f82f576cc52ef93
zouguichu <zouguichu@foreveross.com>
2016-08-08 10:54:53 +0800

    add plugins

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,9 @@
-require("../css/normalize.css");
-var searchArg = require('./search.js');
-console.log(searchArg());
+// define(['jQuery','lodash'],function($,_){
+//import * from 'jQuery';
+    require("../css/normalize.css");
+    var searchArg = require('./search.js');
+    //var showme = $('#showme');
+    //showme = _.toLower(showme)
+    //console.log(showme.show)
+    //console.log(_.toLower('AAAAAA'))
+// })
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,31 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry:{
-        index:'./src/js/index.js'
+    devServer:{
+        contentBase:'./dist/view'
     },
+    entry:[
+        './src/js/index.js'
+    ],
     output:{
         path:path.join(__dirname,'dist/js'),
         filename:'index.js'
     },
     module:{
         loaders:[
-            {test:/\.css$/,loader:'style!css'}
+            {test:/\.css$/,loader:'style!css'},
+            {test:/\.js$/,exclude:'/node_modules/',loader:'babel-loader'}
         ]
-    }
+    },
+    plugins:[
+        new webpack.ProvidePlugin({
+            _:"lodash",
+            $:"jQuery"
+        }),
+        // new webpack.optimize.UglifyJsPlugin({
+        //     compass:{
+        //         warnings:false
+        //     }
+        // })
+    ]
 }
