5679bd1e12e827e2a571f405aa901d6c9e7c1ffa
slimeball <slimeball@foxmail.com>
2017-08-07 19:50:43 +0800

    update

--- js/tools/array.js
+++ js/tools/array.js
@@ -2,9 +2,21 @@
  * 数组工具
  */
 //删除元素
-Array.prototype.remove = function(val){
+Array.prototype.remove = function (val) {
     let index = this.indexOf(val);
-    if(index>-1){
-        this.splice(index,1)
+    if (index > -1) {
+        this.splice(index, 1)
     }
+}
+//去重
+Array.prototype.unique = function () {
+    var res = [];
+    var json = {};
+    for (var i = 0; i < this.length; i++) {
+        if (!json[this[i]]) {
+            res.push(this[i]);
+            json[this[i]] = 1;
+        }
+    }
+    return res;
 }
